52ddaa26b3c2e02532ca73624b3054a1b06ad3c4
John Pollard <johnp@bravelocation.com>
2012-06-03 17:18:04 +0100

    Initial migration of code to GitHub
    Code was previously available at
    https://bitbucket.org/yeltzland/bingtranslateextension

---
+++ bingtranslatescript.js
@@ -0,0 +1,23 @@
+
+function translateOnClick(info, tab) 
+{
+    // Setup the callback function to show the translation
+	window.translatecallback = function(response) {alert(response); };
+	
+    var appId = "66A8CA727C20371BED579D93DC7E476479EAC832";  //--- Replace your own Bing API code here. Register at http://go.microsoft.com/?linkid=9782667
+    var toLanguage = "en";
+    
+    // Encode the text and build the AJAX URL
+    var textToTranslate = encodeURIComponent(info.selectionText);
+    var translateUrl = "http://api.microsofttranslator.com/V2/Ajax.svc/Translate?to=" + toLanguage + "&oncomplete=translatecallback&appid=" + appId + "&text=" + textToTranslate;
+
+    // Setup a script element with the required elements
+    var s = document.createElement("script");
+    s.src = translateUrl;
+    document.getElementsByTagName("head")[0].appendChild(s);
+}
+
+var id = chrome.contextMenus.create({"title": "Bing Translate to English...", 
+                                        "contexts":["selection"],
+                                        "onclick": translateOnClick});
+
---
+++ readme.md
@@ -0,0 +1,6 @@
+# Bing Translate to English Chrome Extension
+Adds a context menu to allow you to send the highlighted text to Bing Translate
+
+##INSTALL DETAILS
+
+You can simply install the extension from the [Chrome Web Store](https://chrome.google.com/webstore/detail/hogdcbncicoifbkfdofpejkkckgkbjig)
